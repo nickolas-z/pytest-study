a4023e03b98be7e1d3e24fa5cb1149c80c555bf9
nickolasz <nickolasz@gmail.com>
2024-06-23 16:33:14 +0300

    add teardown to fixture and try session scope

--- test/sportscar/engine/test_engine.py
+++ test/sportscar/engine/test_engine.py
@@ -7,8 +7,8 @@ from pytest import mark
 def test_can_navigate_to_engine_page(chrome_browser):
     first_browser = chrome_browser
     first_browser.get('https://www.amazon.com/')
-    time.sleep(5)
+    time.sleep(2)
     second_browser = chrome_browser
     second_browser.get('https://www.google.com/')
-    time.sleep(5)
+    time.sleep(2)
     assert True
