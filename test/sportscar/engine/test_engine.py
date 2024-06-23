26f1bbfb5fd2b36f10f887e7008770dd94767ca1
nickolasz <nickolasz@gmail.com>
2024-06-23 15:48:28 +0300

    fixture for open two pages in one browser instance

--- test/sportscar/engine/test_engine.py
+++ test/sportscar/engine/test_engine.py
@@ -1,8 +1,14 @@
+import time
 from pytest import mark
 
 @mark.ui
 @mark.smoke
 @mark.engine
 def test_can_navigate_to_engine_page(chrome_browser):
-    chrome_browser.get('https://www.motortrend.com/theinevitable/')
+    first_browser = chrome_browser
+    first_browser.get('https://www.amazon.com/')
+    time.sleep(5)
+    second_browser = chrome_browser
+    second_browser.get('https://www.google.com/')
+    time.sleep(5)
     assert True
