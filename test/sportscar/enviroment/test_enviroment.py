c833a0eb5cd78a7a44227fc736478a5906bdf909
Nickolasz <nickolasz@gmail.com>
2024-06-24 18:38:57 +0300

    Add @mark.skip
    Add @mark.skip with reason like 'Not a dev environment'

--- test/sportscar/enviroment/test_enviroment.py
+++ test/sportscar/enviroment/test_enviroment.py
@@ -3,16 +3,17 @@ from pytest import mark
 
 @mark.env
 class EnvTests:
+    def test_enviroment_is_dev(self, app_config):
+        assert app_config.base_url == 'https://www.google.com'
+        assert app_config.app_port == 8080
+
+    @mark.skip(reason='Not a dev environment')
     def test_enviroment_is_qa(self, app_config):
-        # if env == 'dev':
-        #     print('This is a dev environment')
-        # assert env == 'qa'
         assert app_config.base_url == 'https://www.amazon.com'
         assert app_config.app_port == 8081
 
-    def test_enviroment_is_dev(self, app_config):
-        # if env == 'qa':
-        #     print('This is a qa environment')
-        # assert env == 'dev'    
-        assert app_config.base_url == 'https://www.google.com'
-        assert app_config.app_port == 8080
+    @mark.skip(reason='Not a staging environment')
+    def test_enviroment_is_staging(self, app_config):
+        base_url = app_config.base_url
+        assert base_url == 'staging'
+        # assert app_config.app_port == 8080
